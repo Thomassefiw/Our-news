a0d3c57453c7dd4c519046d30ab81f03bdc424d2
Tommy_zuckk <sefiwthomas@gmail.com>
2022-10-31 13:25:13 +0300

    initial commit

---
+++ users/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = CustomUser
+
+    list_display = ["email", "username", "age", "is_staff"]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
